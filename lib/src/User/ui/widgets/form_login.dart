02809afd4544aa70ccf9b4a25d25679bbf3f614e
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-09 12:02:06 -0500

    add fields email, password

--- lib/src/User/ui/widgets/form_login.dart
+++ lib/src/User/ui/widgets/form_login.dart
@@ -12,36 +12,103 @@ class FormLogin extends StatelessWidget {
       margin: EdgeInsets.only(top: size.height * .12, right: 30.0, left: 30.0),
       child: Column(
         children: [
-          Container(
-            margin: EdgeInsets.only(bottom: 30.0),
-            child: Column(
-              children: [
-                Icon( Icons.person_pin_circle, color: Colors.white, size: 80.0,),
-                Text('Bryan Rodríguez', style: TextStyle(color: Colors.white, fontSize: 30.0),)
-              ],
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            height: 400.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(.4),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3)
-                )
-              ]
-            ),
-            child: Text('INGRESO USUARIO')
-          ),
+          _pinUser(),
+          _boxForm(),
           SizedBox(height: 30.0,),
           Text('¿Olvidaste tu contraseña?')
         ],
       ),
+    );
+  }
+
+  Widget _pinUser(){
+    return Container(
+      margin: EdgeInsets.only(bottom: 30.0),
+      child: Column(
+        children: [
+          Icon( Icons.person_pin_circle, color: Colors.white, size: 80.0,),
+          Text('Bryan Rodríguez', style: TextStyle(color: Colors.white, fontSize: 30.0),)
+        ],
+      ),
+    );
+  }
+
+  Widget _boxForm(){
+    return Container(
+      alignment: Alignment.center,
+      height: 400.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(.4),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3)
+          )
+        ]
+      ),
+      child: Container(
+        margin: EdgeInsets.only(top: 0.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('INGRESO' , style: TextStyle(fontSize: 20.0, color: Colors.deepPurple, fontWeight: FontWeight.bold),),
+             SizedBox(height: 30.0,),
+            _createEmail(),
+            SizedBox(height: 30.0,),
+            _createPassword(),
+             SizedBox(height: 30.0,),
+             _loginButton()
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _createEmail(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon( Icons.alternate_email, color: Colors.deepPurple ),
+          hintText: 'ejemplo@correo.com',
+          labelText: 'Correo electrónico',
+        ),
+        onChanged: (value){},
+      ),
+    );
+  }
+
+  Widget _createPassword(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon( Icons.lock_outline, color: Colors.deepPurple ),
+          labelText: 'Contraseña',
+        ),
+        onChanged: (value){},
+      ),
+    );
+  }
+
+  Widget _loginButton(){
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric( horizontal: 80.0, vertical: 15.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0)
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: () {}
     );
   }
 }
