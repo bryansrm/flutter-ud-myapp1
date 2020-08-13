9bce7d6f8b389e161bbb2ea9edc903188314d4af
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-12 22:46:19 -0500

    create form product

--- lib/src/Producto/ui/pages/product_page.dart
+++ lib/src/Producto/ui/pages/product_page.dart
@@ -9,8 +9,14 @@ class ProductPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Product page'),
         actions: [
-          Icon( Icons.image ),
-          Icon( Icons.camera_alt )
+          IconButton( 
+            icon: Icon( Icons.image ),
+            onPressed: (){},
+           ),
+          IconButton( 
+            icon: Icon( Icons.camera_alt ),
+            onPressed: (){},
+           ),
         ],
       ),
       body: _contentPage(),
@@ -18,13 +24,52 @@ class ProductPage extends StatelessWidget {
   }
 
   Widget _contentPage(){
-    return Center(
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
       child: Column(
         children: [
-          Text( 'PRODUCT PAGE' )
-          // _inputProduct(),
-          // _inputPrice(),
+          Form(
+            child: Column(
+              children: [
+                _inputProduct(),
+                _inputPrice(),
+                SizedBox(height: 20.0,),
+                _submitButton()
+              ],
+            ),
+          )
         ],
+      ),
+    );
+  }
+
+  Widget _inputProduct(){
+    return TextFormField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Product'
+      ),
+    );
+  }
+
+  Widget _inputPrice(){
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: 'Price'
+      ),
+    );
+  }
+
+  Widget _submitButton(){
+    return RaisedButton.icon(
+      onPressed: (){}, 
+      color: globalstyles.primaryColor,
+      textColor: Colors.white,
+      icon: Icon( Icons.save ), 
+      label: Text('Save'),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0)
       ),
     );
   }
