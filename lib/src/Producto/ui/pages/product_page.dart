81b831f08f8ac7ba1e67d829c06748ce63804ff9
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-18 22:30:47 -0500

    create model product

--- lib/src/Producto/ui/pages/product_page.dart
+++ lib/src/Producto/ui/pages/product_page.dart
@@ -1,11 +1,19 @@
+import 'package:app_udemy_1/src/models/producto_model.dart';
 import 'package:flutter/material.dart';
 
 import 'package:app_udemy_1/src/Util/global_styles.dart' as globalstyles;
 import 'package:app_udemy_1/src/Util/validations.dart' as validations;
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
 
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
   final formKey = GlobalKey<FormState>();
+
+  ProductoModel producto = new ProductoModel();
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +46,7 @@ class ProductPage extends StatelessWidget {
               children: [
                 _inputProduct(),
                 _inputPrice(),
+                _createAvailable(),
                 SizedBox(height: 20.0,),
                 _submitButton()
               ],
@@ -50,19 +59,23 @@ class ProductPage extends StatelessWidget {
 
   Widget _inputProduct(){
     return TextFormField(
+      initialValue: producto.titulo,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         labelText: 'Product'
       ),
+      onSaved: (value) => producto.titulo = value,
     );
   }
 
   Widget _inputPrice(){
     return TextFormField(
+      initialValue: producto.valor.toString(),
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
         labelText: 'Price'
       ),
+      onSaved: (value) => producto.valor = double.parse(value),
       validator: ( value ){
 
         if( value.isEmpty ){
@@ -91,8 +104,24 @@ class ProductPage extends StatelessWidget {
     );
   }
 
+  Widget _createAvailable(){
+    return SwitchListTile(
+      value: producto.disponible,
+      title: Text('Disponible'),
+      onChanged: ( value ) {
+        setState(() {
+          producto.disponible = value;
+        });
+      },
+    );
+  }
+
   void _submitFunction(){
     if( !formKey.currentState.validate() ) return null;
+
+    formKey.currentState.save();
     print('avanzar');
+    print(producto.titulo);
+    print(producto.valor);
   }
 }
